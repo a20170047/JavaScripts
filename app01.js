66681cc73f81051ccc001af81d99ab0f690741be
a20170047 <a20170047@juan23.edu.pe>
2024-10-23 20:54:29 -0500

    Add files via upload

---
+++ app01.js
@@ -0,0 +1,5 @@
+document.write("<h1>Bienvenidos a la pagina Java Script</h1>");
+var nom = prompt("Ingrese su nombre");
+document.write(
+  "Hola " + nom + " es tu primer contacto con la programacion en JavaScript"
+);
